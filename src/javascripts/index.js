12eb0ef5fc7407f2a73672eff832898e0e739d96
mosson <cucation@gmail.com>
2015-11-12 21:31:43 +0900

    hit

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
-
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactDOMServer = require('react-dom/server');
+const App = require('components/index/App');
 const global = Function("return this")();
 const document = global.document;
 
 
-document.getElementById('App').innerHTML = 'Hello, World';
+document.getElementById('App').innerHTML = 'Hello, Problem!';
 
 
