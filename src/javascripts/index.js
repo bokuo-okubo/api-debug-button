1b1ce7158df39b333c37ba3f7318270afceea58d
mosson <cucation@gmail.com>
2015-11-12 21:30:08 +0900

    remove react

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const React = require('react');
-const ReactDOM = require('react-dom');
-const ReactDOMServer = require('react-dom/server');
-const App = require('components/index/App');
+
 const global = Function("return this")();
 const document = global.document;
 
-ReactDOM.render(<App/>, document.getElementById('App'));
+
+document.getElementById('App').innerHTML = 'Hello, World';
 
 
